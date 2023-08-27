0e0531648e73f39aae252974f86d4c051c8926a8
Mester re <bogdan2010gladiy@gmail.com>
2023-08-27 19:43:27 -0400

    add main folder

--- includes/own/checkers_ai_start_checkers.cpp
+++
@@ -1,9 +0,0 @@
-#include "includes/checkers/checkers_ai/Application_checkers.h"
-
-void checkers_ai_start()
-{
-    // Calls Application consturctor
-    Application_checkers application;
-
-    application.startApplication();
-}
--- includes/own/includes/battle_city/source/Base_battle_city.cpp
+++
@@ -1,9 +0,0 @@
-#include "Base_battle_city.h"
-
-Base::Base()
-    : life(true) {
-    mTexture.loadFromFile("includes/battle_city/media/baseSprite.png");
-    mSprite.setTexture(mTexture);
-    mSprite.setTextureRect(sf::IntRect(0, 0, 48, 48));
-    mSprite.setPosition(336, 600);
-}
