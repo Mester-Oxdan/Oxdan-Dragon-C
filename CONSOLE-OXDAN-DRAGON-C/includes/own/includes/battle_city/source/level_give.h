1e7bba7d7b92a7b62b8f7f5f027f2cea26b2acad
Mester re <bogdan2010gladiy@gmail.com>
2025-07-24 11:34:28 -0400

    overwriting

--- CONSOLE-OXDAN-DRAGON-C/includes/own/includes/battle_city/source/level_give.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "Application.h"
-#include "Map.h"
-
-int level_batl_city = 1;
--- CONSOLE-OXDAN-DRAGON-C/includes/own/includes/chess/chess_2/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "chess.h"
-
-int main_2()
-{
-	Chess chess;
-	chess.game();
-
-	return 0;
-}
